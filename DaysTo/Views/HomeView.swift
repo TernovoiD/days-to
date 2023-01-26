095e1375a599b93056768ebc6c015d189f729fb9
Danylo Ternovoi <121759315+TernovoiD@users.noreply.github.com>
2023-01-26 16:27:11 +0200

    Created basic UI

--- DaysTo/Views/HomeView.swift
+++ DaysTo/Views/HomeView.swift
@@ -8,10 +8,30 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    @EnvironmentObject var eventsVM: EventsViewModel
+    @State var showAddEventView: Bool = false
+    @State var search: String = ""
+    
     var body: some View {
-        VStack {
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            Image(systemName: "globe")
+        NavigationView {
+            ScrollView {
+                ForEach(eventsVM.events) { event in
+                    EventRowView(event: event)
+                }
+            }
+            .padding(.horizontal)
+            .shadow(color: .black, radius: 3, y: 3)
+            .searchable(text: $search)
+            .navigationTitle("Upcoming")
+            .toolbar {
+                ToolbarItem {
+                    addEventButton
+                }
+            }
+            .sheet(isPresented: $showAddEventView) {
+                AddEventView(isPresented: $showAddEventView)
+            }
         }
     }
 }
@@ -19,5 +39,17 @@ struct HomeView: View {
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
+            .environmentObject(EventsViewModel())
+    }
+}
+
+extension HomeView {
+    
+    var addEventButton: some View {
+        Button {
+            showAddEventView = true
+        } label: {
+            Image(systemName: "plus")
+        }
     }
 }
